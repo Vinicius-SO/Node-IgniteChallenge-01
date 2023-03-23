025b1e79b0ad0801dfd2392871ff4e018cd3497a
Vinicius-SO <vhisoares39@gmail.com>
2023-03-23 10:27:57 -0300

    POST route

--- src/server.js
+++ src/server.js
@@ -1,19 +1,28 @@
 import http from "node:http"
+import { json } from "./middlewares/json.js"
 
 const tasks = []
 
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async(req,res)=>{
     
-    const { method, url} = req
+    const { method, url, body} = req
+
+    await json(req,res)
 
     console.log(method,url)
 
     if(method === 'GET' && url === '/tasks'){
         return res
-        .setHeader('content-type','application/json')
+        .setHeader('Content-Type','application/json')
         .end(JSON.stringify(tasks))
     } 
+
+    if(method === 'POST' && url === '/tasks'){
+        const {body} = req
+        tasks.push(body)
+        // return res.writeHead(204)
+    }
     
     
     return res.end().writeHead(404)
